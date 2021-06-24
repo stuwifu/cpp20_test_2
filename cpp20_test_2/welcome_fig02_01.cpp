725a1d4731ee1c70e22db3f2564a42c0002d3877
Stuart Irwin <stuartdi@hotmail.com>
2021-06-24 16:54:46 -0700

    where do I put this?

---
+++ cpp20_test_2/welcome_fig02_01.cpp
@@ -0,0 +1,10 @@
+// welcome_fig02_01.cpp
+// text printing application
+#include <iostream>  // enables output
+
+// function main begins application execution
+int main() {
+    std::cout << "Welcome to C++!\n";  // display msg
+
+    return 0;  // did it end successfully?
+} // end function main
